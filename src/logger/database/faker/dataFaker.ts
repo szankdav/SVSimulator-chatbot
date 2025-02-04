db8bd03844472bf98c3ca4461900c939ac799614
David Szankovszky <szankovszky.david@gmail.com>
2025-02-04 16:43:09 +0100

    Faker added

--- src/logger/database/faker/dataFaker.ts
+++ src/logger/database/faker/dataFaker.ts
@@ -2,15 +2,18 @@ import { faker } from '@faker-js/faker';
 import { AuthorModel } from "../../model/author.model";
 import { createAuthorController } from "../../controller/author.controller";
 import { Database } from 'sqlite3';
+import { MessageModel } from '../../model/message.model';
 
 function createRandomAuthor(): AuthorModel {
     const id = 0;
     const name = faker.person.fullName();
     const createdAt = faker.date.past().toLocaleString();
 
-    return {id, name, createdAt};
+    return { id, name, createdAt };
 }
 
 export async function createRandomAuthorsInDatabase(db: Database): Promise<void> {
-    await createAuthorController(db, createRandomAuthor());
+    for (let i = 0; i < 100; i++) {
+        await createAuthorController(db, createRandomAuthor());
+    }
 } 
