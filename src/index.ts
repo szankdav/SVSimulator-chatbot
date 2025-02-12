59aba193544457d035e85b890970841984e007eb
David Szankovszky <szankovszky.david@gmail.com>
2025-02-12 10:19:45 +0100

    home path changed to /

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-app.get('/home', homeHandler);
+app.get('/', homeHandler);
 app.get('/authors/:page', authorsHandler);
 app.get('/messages/:page', messagesHandler);
 app.get('/messages/author/:id', messagesByAuthorsHandler);
